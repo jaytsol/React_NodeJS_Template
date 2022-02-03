51d98e5c94c0a0e01db5f62c167f2c2a67f45601
Jay <jaewhee@tictag.io>
2022-02-03 18:14:07 +0900

    init

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+PORT = process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@reactnode.ykm1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err)),
+  app.get('/', (req, res) => res.send('Hello World!!!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
