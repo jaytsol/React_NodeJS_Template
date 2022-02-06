4a4ee30c6051052537befdce556772af46205a5c
Jay <jaewhee@tictag.io>
2022-02-06 15:50:58 +0900

    Password bcrypted

--- models/User.js
+++ models/User.js
@@ -1,32 +1,48 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
+const bcrpyt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+    bcrpyt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
 
-module.exports = { User }
+      bcrpyt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
